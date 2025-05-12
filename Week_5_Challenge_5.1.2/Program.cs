ead08fe7e43656d9c14b5eaa250a0b66885abaef
kende <kende@KENNY>
2025-05-12 08:26:32 -0500

    Add project files.

---
+++ Week_5_Challenge_5.1.2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Week_5_Challenge_5._1._2
+{
+    internal class Program
+    {
+        static int MissingNumber(int[] nums)
+        {
+            int n = nums.Length;
+            int expected = n * (n + 1) / 2;
+            int actualSum = 0;
+            foreach (int x in nums)
+                actualSum += x;
+            return expected - actualSum;
+        }
+
+        static void Main(string[] args)
+        {
+            // Example 1:
+            int[] nums = { 3, 0, 1 };
+
+            // Call the helper and print the missing number:
+            Console.WriteLine(MissingNumber(nums));  // Output: 2
+
+        }
+    }
+}
---
+++ Week_5_Challenge_Lab_5.1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Week_5_Challenge_Lab_5._1
+{
+    internal class Program
+    {
+        //Given a non-empty array of integers nums, every element appears twice except for one.Find that single one.
+        //Example 1:
+        //Input: nums = [2, 2, 1]
+        //Output: 1
+        static int UniqueEle(int[] nums)
+        {
+            HashSet<int> set = new HashSet<int>();
+
+            foreach (int num in nums)
+            {
+                if (set.Contains(num))
+                {
+                    set.Remove(num);
+                }
+                else
+                    set.Add(num);
+            }
+
+            foreach(int unique in set)
+            {
+                return unique;
+            }
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            int[] nums = {2, 2, 1};
+            int result = UniqueEle(nums);
+            Console.WriteLine(result);
+
+        }
+    }
+}
